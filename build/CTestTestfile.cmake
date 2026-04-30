271a4fc2f89c39cd849868099854dc6c4f457ac7
Akshay-2007-1 <akshayvemulapalli2007@gmail.com>
2026-05-01 07:11:49 +0800

    chore: add .gitignore, remove build folder from tracking

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vakshay/exchange-engine
-# Build directory: /home/vakshay/exchange-engine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(OrderBookTests "/home/vakshay/exchange-engine/build/tests")
-set_tests_properties(OrderBookTests PROPERTIES  _BACKTRACE_TRIPLES "/home/vakshay/exchange-engine/CMakeLists.txt;25;add_test;/home/vakshay/exchange-engine/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
